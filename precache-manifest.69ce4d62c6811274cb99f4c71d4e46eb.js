99b83f6bb16924f29ce6d76f39fd2bd4010e3594
Paweł Dobrowolski <pawwel3@gmail.com>
2020-10-27 12:32:41 +0100

    Updates

---
+++ precache-manifest.69ce4d62c6811274cb99f4c71d4e46eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f68e180e5456d8c594db8c348d8961",
+    "url": "/cosmolight/index.html"
+  },
+  {
+    "revision": "5f06ecaed8e7d2b7ca54",
+    "url": "/cosmolight/static/css/main.911c92de.chunk.css"
+  },
+  {
+    "revision": "8588dd471e2c3fb0fe61",
+    "url": "/cosmolight/static/js/2.a16d6a24.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/cosmolight/static/js/2.a16d6a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f06ecaed8e7d2b7ca54",
+    "url": "/cosmolight/static/js/main.b2bd3cee.chunk.js"
+  },
+  {
+    "revision": "74961ccad5f52ee3c122",
+    "url": "/cosmolight/static/js/runtime-main.d2bd6188.js"
+  },
+  {
+    "revision": "b7c1c488477da2450839aefb95c71de3",
+    "url": "/cosmolight/static/media/Instagram.b7c1c488.svg"
+  },
+  {
+    "revision": "1cfbfd2d3473a450dec2c843442b50d8",
+    "url": "/cosmolight/static/media/Warstwa_x0020_1.1cfbfd2d.svg"
+  },
+  {
+    "revision": "8a6c91317cf74c5960aca7a571da6096",
+    "url": "/cosmolight/static/media/new_york_p04380wh_4.8a6c9131.png"
+  },
+  {
+    "revision": "ab9b4707cc7dbe5c3cced04b25360238",
+    "url": "/cosmolight/static/media/v.ab9b4707.svg"
+  },
+  {
+    "revision": "75eb7e10f6321510cdcffb9b6e9f257b",
+    "url": "/cosmolight/static/media/washington_p05915wh.75eb7e10.png"
+  },
+  {
+    "revision": "395418edb8b92b899fb37446d23723c7",
+    "url": "/cosmolight/static/media/yt.395418ed.svg"
+  }
+]);
