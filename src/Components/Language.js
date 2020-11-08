b55da2b6b4ba3cd5bb31c000fe537762da5dfa4a
Paweł Dobrowolski <pawwel3@gmail.com>
2020-11-08 16:08:50 +0100

    change handlerIsPl

--- src/Components/Language.js
+++ src/Components/Language.js
@@ -2,21 +2,13 @@ import React from "react";
 import "../style/Language.css";
 const Language = () => {
   const [isPl, setIsPl] = React.useState(true);
-  const [language, setLanguage] = React.useState("pl");
-  const handlerIsPl = () => {
-    if (isPl === true) {
-      setIsPl(!isPl);
-      setLanguage("en");
-    } else {
-      setIsPl(!isPl);
-      setLanguage("pl");
-    }
-  };
+  const handlerIsPl=()=>setIsPl(isPl=>!isPl)
+  
   return (
     <div className="contener">
       <div className="toggle-btn" onClick={handlerIsPl}>
-        <div className={language}>
-          <div className="pl-en">{language}</div>
+        <div className={isPl?'pl':'en'}>
+          <div className="pl-en">{isPl?'pl':'en'}</div>
         </div>
       </div>
     </div>
